bb4df40ac94cbaa50a75463a19accf5e10aa6e79
Amelia Gäßler <brunwault@gmail.com>
2024-11-10 14:39:49 +0200

    added external

---
+++ include/glue/external.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+// std
+#include <vector>
+
+namespace glue {
+	void process(std::vector<interface *> interface_objects, const unsigned int screen_width, const unsigned int screen_height)
+	{
+		for (interface *intf: interface_objects)
+		{
+			if (nullptr == intf->get_parent())
+			{
+				intf->calculate_screen_metrics(vec2(0, 0), screen_width, screen_height);
+			}
+		}
+	}
+}
