518cd4644d22cab4873d56acbce196aa85d2dfe5
Amelia Gäßler <brunwault@gmail.com>
2024-11-12 12:14:25 +0200

    added interface_type enum

--- include/glue/interface.hpp
+++ include/glue/interface.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "./interface/type_enum.hpp"
 #include "./interface/type_interface.hpp"
 #include "./interface/type_button.hpp"
 #include "./interface/type_textured.hpp"
---
+++ include/glue/interface/type_enum.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace glue
+{
+	enum interface_type
+	{
+		_INTERFACE,
+		_TEXTURED_INTERFACE,
+		_BUTTON_INTERFACE,
+	};
+}
