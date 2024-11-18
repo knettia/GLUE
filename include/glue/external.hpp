ab4e9d1d3e5e4a80bd26e81ad0cd7aeef8756125
Amelia Gäßler <brunwault@gmail.com>
2024-11-18 23:44:24 +0200

    added basic sorting

--- include/glue/external.hpp
+++ include/glue/external.hpp
@@ -4,13 +4,20 @@
 #include <vector>
 
 namespace glue {
-	void process(std::vector<interface *> interface_objects, const unsigned int screen_width, const unsigned int screen_height)
+	void process(std::vector<interface *> &interface_objects, const unsigned int screen_width, const unsigned int screen_height)
 	{
+		std::sort(interface_objects.begin(), interface_objects.end(), 
+		[](const glue::interface *a, const glue::interface *b)
+		{
+			return a->z_layer < b->z_layer;
+		});
+
+
 		for (interface *intf: interface_objects)
 		{
 			if (nullptr == intf->get_parent())
 			{
-				intf->calculate_screen_metrics(vec2(0, 0), screen_width, screen_height);
+				intf->calculate_screen_metrics(screen_width, screen_height);
 			}
 		}
 	}
